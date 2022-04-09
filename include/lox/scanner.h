8470c1805aac060603f3393b310a0cf06122f87a
Darko Janekovic <darko.janekovic@gmail.com>
2022-04-09 18:27:00 +0200

    tidy up scanner
    Signed-off-by: Darko Janekovic <darko.janekovic@gmail.com>

--- include/lox/scanner.h
+++ include/lox/scanner.h
@@ -7,55 +7,49 @@
 
 namespace lox {
 class Scanner {
-private:
-    std::size_t start = 0;
-    std::size_t current = 0;
-    std::size_t line = 1;
+  public:
+    explicit Scanner(std::string&& program);
+    std::vector<Token> scan_tokens();
+
+  private:
+    std::size_t start_ = 0;
+    std::size_t current_ = 0;
+    std::size_t line_ = 1;
 
     std::vector<Token> tokens_;
     std::string program_;
-    std::unordered_map<std::string, TokenType> keywords_;
+    std::unordered_map<std::string, TokenType> keywords_{
+      {"and", TokenType::AND},
+      {"class", TokenType::CLASS},
+      {"else", TokenType::ELSE},
+      {"false", TokenType::FALSE},
+      {"for", TokenType::FOR},
+      {"fun", TokenType::FUN},
+      {"if", TokenType::IF},
+      {"nil", TokenType::NIL},
+      {"or", TokenType::OR},
+      {"print", TokenType::PRINT},
+      {"return", TokenType::RETURN},
+      {"super", TokenType::SUPER},
+      {"this", TokenType::THIS},
+      {"true", TokenType::TRUE},
+      {"var", TokenType::VAR},
+      {"while", TokenType::WHILE}};
 
     bool is_end() const;
     bool match(const char c);
-    void add_token(const TokenType type);
+    std::optional<Token> scan_token();
 
     // Call this function for tokens that have lexemes we need to remember
     // IDENTIFIER, STRING and NUMBER
     template<typename LexemeType>
-    void add_token(const TokenType type, LexemeType&& lexeme)
-    {
-        tokens_.emplace_back(type, std::forward<LexemeType>(lexeme), line);
-    }
+    Token get_current_line_token(const TokenType type, LexemeType&& lexeme) const;
+    Token get_current_line_token(const TokenType type) const;
 
     char advance();
-    void scan_token();
-    template<size_t LookAhead> char peek() const;
-    void string();
-    void number();
-    void identifier();
-
-public:
-    explicit Scanner(std::string&& program):
-        program_{std::move(program)},
-        keywords_{
-            {"and", TokenType::AND},
-            {"class", TokenType::CLASS},
-            {"else", TokenType::ELSE},
-            {"false", TokenType::FALSE},
-            {"for", TokenType::FOR},
-            {"fun", TokenType::FUN},
-            {"if", TokenType::IF},
-            {"nil", TokenType::NIL},
-            {"or", TokenType::OR},
-            {"print", TokenType::PRINT},
-            {"return", TokenType::RETURN},
-            {"super", TokenType::SUPER},
-            {"this", TokenType::THIS},
-            {"true", TokenType::TRUE},
-            {"var", TokenType::VAR},
-            {"while", TokenType::WHILE}} {}
-
-    std::vector<Token> scan_tokens();
+    char peek(std::size_t look_ahead) const;
+    std::optional<Token> string();
+    Token number();
+    Token identifier();
 };
 } // namespace lox
--- src/scanner.cpp
+++ src/scanner.cpp
@@ -7,149 +7,154 @@
 
 using namespace lox;
 
-void Scanner::add_token(const TokenType type)
+Scanner::Scanner(std::string&& program):
+    program_{std::move(program)} {}
+
+std::vector<Token> Scanner::scan_tokens()
+{
+    while(!is_end()) {
+        start_ = current_;
+        if (auto token = scan_token()) {
+            tokens_.push_back(std::move(*token));
+        }
+    }
+
+    tokens_.emplace_back(TokenType::END, line_);
+    return tokens_;
+}
+
+Token Scanner::get_current_line_token(const TokenType type) const
 {
     // These are the only tokens that need lexemes
     if (type == TokenType::IDENTIFIER or type == TokenType::STRING or type == TokenType::NUMBER) {
-        std::string lexeme = program_.substr(start, current-start);
+        std::string lexeme = program_.substr(start_, current_-start_);
         if (type == TokenType::NUMBER) {
-            return add_token(type, std::stod(lexeme));
+            return get_current_line_token(type, std::stod(lexeme));
         } else if (type == TokenType::STRING) {
             // skip first " and total string has length: size - 2
             lexeme = lexeme.substr(1, lexeme.size() - 2);
         }
-        return add_token(type, std::move(lexeme));
+        return get_current_line_token(type, std::move(lexeme));
     }
 
     // we don't need lexeme so just pass std::monostate as nothing
-    add_token(type, std::monostate());
+    return get_current_line_token(type, std::monostate());
+}
+
+
+template<typename LexemeType>
+Token Scanner::get_current_line_token(const TokenType type, LexemeType&& lexeme) const
+{
+    return Token{type, std::forward<LexemeType>(lexeme), line_};
 }
 
 char Scanner::advance()
 {
-    return program_.at(current++);
+    return program_.at(current_++);
 }
 
-void Scanner::scan_token()
+std::optional<Token> Scanner::scan_token()
 {
     char c = advance();
     switch(c) {
         case '(':
-            add_token(TokenType::LEFT_PAREN);
-            break;
+            return get_current_line_token(TokenType::LEFT_PAREN);
         case ')':
-            add_token(TokenType::RIGHT_PAREN);
-            break;
+            return get_current_line_token(TokenType::RIGHT_PAREN);
         case '{':
-            add_token(TokenType::LEFT_BRACE);
-            break;
+            return get_current_line_token(TokenType::LEFT_BRACE);
         case '}':
-            add_token(TokenType::RIGHT_BRACE);
-            break;
+            return get_current_line_token(TokenType::RIGHT_BRACE);
         case ',':
-            add_token(TokenType::COMMA);
-            break;
+            return get_current_line_token(TokenType::COMMA);
         case '.':
-            add_token(TokenType::DOT);
-            break;
+            return get_current_line_token(TokenType::DOT);
         case '-':
-            add_token(TokenType::MINUS);
-            break;
+            return get_current_line_token(TokenType::MINUS);
         case '+':
-            add_token(TokenType::PLUS);
-            break;
+            return get_current_line_token(TokenType::PLUS);
         case ';':
-            add_token(TokenType::SEMICOLON);
-            break;
+            return get_current_line_token(TokenType::SEMICOLON);
         case '*':
-            add_token(TokenType::STAR);
-            break;
+            return get_current_line_token(TokenType::STAR);
         case '!':
-            add_token(match('=') ? TokenType::BANG_EQUAL : TokenType::BANG);
-            break;
+            return match('=') ? get_current_line_token(TokenType::BANG_EQUAL) : get_current_line_token(TokenType::BANG);
         case '=':
-            add_token(match('=') ? TokenType::EQUAL_EQUAL : TokenType::EQUAL);
-            break;
+            return match('=') ? get_current_line_token(TokenType::EQUAL_EQUAL) : get_current_line_token(TokenType::EQUAL);
         case '<':
-            add_token(match('=') ? TokenType::LESS_EQUAL : TokenType::LESS);
-            break;
+            return match('=') ? get_current_line_token(TokenType::LESS_EQUAL) : get_current_line_token(TokenType::LESS);
         case '>':
-            add_token(match('=') ? TokenType::GREATER_EQUAL : TokenType::GREATER);
-            break;
+            return match('=') ? get_current_line_token(TokenType::GREATER_EQUAL) : get_current_line_token(TokenType::GREATER);
         case '/':
             if (match('/')) {
-                for(; peek<0>() != '\n' && !is_end(); advance());
-            } else {
-                add_token(TokenType::SLASH);
+                for(; peek(0) != '\n' && !is_end(); advance());
+                return std::nullopt;
             }
-            break;
+            return get_current_line_token(TokenType::SLASH);
         case ' ':
         case '\r':
         case '\t':
             //ignore
-            break;
+            return std::nullopt;
         case '\n':
-            line++;
-            break;
+            line_++;
+            return std::nullopt;
         case '"':
-            string();
-            break;
+            return string();
         default:
             if (std::isdigit(c)) {
-                number();
+                return number();
             } else if (std::isalnum(c) || c == '_') {
-                identifier();
-            } else {
-                Lox::error(line, "Unexpected character");
+                return identifier();
             }
-            break;
+            Lox::error(line_, "Unexpected character");
+            return std::nullopt;
     }
 }
 
-void Scanner::identifier()
+Token Scanner::identifier()
 {
-    for(; std::isalnum(peek<0>()) || peek<0>() == '_'; advance());
+    for(; std::isalnum(peek(0)) || peek(0) == '_'; advance());
 
-    const std::string text = program_.substr(start, current-start);
+    const std::string text = program_.substr(start_, current_-start_);
     const auto it = keywords_.find(text);
-    add_token(it != keywords_.end() ? it->second : TokenType::IDENTIFIER);
+    return get_current_line_token(it != keywords_.end() ? it->second : TokenType::IDENTIFIER);
 }
 
-void Scanner::number()
+Token Scanner::number()
 {
-    for(; isdigit(peek<0>()); advance());
+    for(; isdigit(peek(0)); advance());
 
     // check if the number is real or just integer
-    if (peek<0>() == '.' && isdigit(peek<1>())) {
+    if (peek(0) == '.' && isdigit(peek(1))) {
         advance();
-        for (; isdigit(peek<0>()); advance());
+        for (; isdigit(peek(0)); advance());
     }
 
-    add_token(TokenType::NUMBER);
+    return get_current_line_token(TokenType::NUMBER);
 }
 
 
-void Scanner::string()
+std::optional<Token> Scanner::string()
 {
-    for (; peek<0>() != '"' && !is_end(); advance()) {
-        if (peek<0>() == '\n') {
-            line++;
+    for (; peek(0) != '"' && !is_end(); advance()) {
+        if (peek(0) == '\n') {
+            line_++;
         }
     }
 
     if (is_end()) {
-        Lox::error(line, "Unterminated string!");
-        return;
+        Lox::error(line_, "Unterminated string!");
+        return std::nullopt;
     }
 
     // eat last "
     advance();
-    add_token(TokenType::STRING);
+    return get_current_line_token(TokenType::STRING);
 }
 
-template<size_t LookAhead>
-char Scanner::peek() const {
-    return (current + LookAhead) >= program_.size() ? '\0' : program_.at(current + LookAhead);
+char Scanner::peek(std::size_t look_ahead) const {
+    return (current_ + look_ahead) >= program_.size() ? '\0' : program_.at(current_ + look_ahead);
 }
 
 bool Scanner::match(const char c) {
@@ -157,25 +162,15 @@ bool Scanner::match(const char c) {
         return false;
     }
 
-    if (program_.at(current) != c) {
+    if (program_.at(current_) != c) {
         return false;
     }
 
-    current++;
+    current_++;
     return true;
 }
 
 bool Scanner::is_end() const {
-    return current >= program_.size();
+    return current_ >= program_.size();
 }
 
-std::vector<Token> Scanner::scan_tokens()
-{
-    while(!is_end()) {
-        start = current;
-        scan_token();
-    }
-
-    tokens_.emplace_back(TokenType::END, line);
-    return tokens_;
-}
